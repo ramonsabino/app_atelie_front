2c1abb21d081233e49fefdc38a162f956b197552
ramonsabino <ramon-sabino@hotmail.com>
2024-06-15 17:54:43 -0300

    feat: integração com back

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://app-atelie-back-1.onrender.com:5000', // Base URL do seu backend
+  baseURL: 'https://app-atelie-back.onrender.com', // Base URL do seu backend
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
 export default api;
